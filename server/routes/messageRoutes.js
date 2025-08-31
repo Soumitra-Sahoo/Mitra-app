e40367da5d442594301ed0da8a8a8cf325149040
Your Name <soumitrasahoo030@gmail.com>
2025-08-31 11:51:55 +0530

    Error fix

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -4,8 +4,8 @@ import { upload } from '../configs/multer.js';
 import { protect } from '../middlewares/auth.js';
 
 const messageRouter = express.Router();
-
 messageRouter.get('/:userId', sseController);
+
 messageRouter.post('/send', upload.single('image'), protect, sendMessage);
 messageRouter.post('/get', protect, getChatMessages);
 
